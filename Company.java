3199c4851a46c62843660267badf5443dd20ec76
BartekKRaW <166992335+BartekKRaW@users.noreply.github.com>
2024-04-14 12:31:17 +0200

    Update Company.java

--- Company.java
+++ Company.java
@@ -1,17 +1,20 @@
 public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
+          int nonManagerCount = 0;
 
-        employees[0] = new Employee("Marek Stuard", 2000);
+        employees[0] = new Manager("Marek Stuard", 2000);
         employees[1] = new Employee("Wojtek Jesionka", 6500);
-        employees[2] = new Employee("Bob Marley", 1300);
+        employees[2] = new Worker("Bob Marley", 1300);
         employees[3] = new Employee("Grzegorz Kogucik", 9800);
-        employees[4] = new Employee("Michał Szpak", 5000);
-      
-        System.out.println("Dane dla pracownika o indeksie 3:");
-        System.out.println(employees[3]);
-        employees[3].setSalary(10000);
-
+        employees[4] = new Worker("Michał Szpak", 5000);
+        for (Employee employee : employees) {
+            if (!(employee instanceof Manager)) {
+                nonManagerCount++;
+            }
+        }
+        ((Manager)employees[0]).setNumberOfSubordinates(nonManagerCount);
+        employees[0].setSalary(7500);
         System.out.println("\nDane dla wszystkich pracowników:");
         for (Employee employee : employees) {
             System.out.println(employee);
