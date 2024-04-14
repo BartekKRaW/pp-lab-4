29758bc1fa95d44d927123629fd59c1e0d6bb153
BartekKRaW <166992335+BartekKRaW@users.noreply.github.com>
2024-04-14 12:21:57 +0200

    Update Worker.java

--- employees/Worker.java
+++ employees/Worker.java
@@ -6,15 +6,12 @@ public class Worker extends Employee {
     public Worker(String fullName, double salary) {
         super(fullName, salary);
     }
-    
     public String getPosition(){
         return position;
     }
-
     public void setPosition(String position){
         this.position = position;
     }
-
     @Override
     public String toString() {
         return "Manager: " + getName() + ", Salary: " + getSalary() + ", Position: " + position;
